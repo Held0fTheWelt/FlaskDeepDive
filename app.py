46be1d5ee70634dc0cba4acec4ea6960f145699a
Yves Tanas <Yves.Tanas@gmx.de>
2026-03-03 16:27:42 +0100

    Control Structures
    Added a dictionary
    Changed the index.html template to display additional information based on the name of the user
    Added an additional template and and additional route called /all-users.

--- app.py
+++ app.py
@@ -3,6 +3,12 @@ from datetime import datetime
 
 app = Flask(__name__, template_folder='template')
 
+users = {
+    'Alice': {'age': 25, 'country': 'USA'},
+    'Bob': {'age': 30, 'country': 'UK'},
+    'Charlie': {'age': 35, 'country': 'Australia'}
+}
+
 
 @app.route('/')
 def index():
@@ -15,13 +21,20 @@ def index():
         'index.html',
         title='Home',
         user=name,
-        current_time=datetime.utcnow()
+        current_time=datetime.utcnow(),
+        users=users
     )
 
 
 @app.route('/form')
 def form():
     return render_template('form.html')
+
+
+@app.route('/all-users')
+@app.route('/all_users')  # alternative URL (underscore)
+def all_users():
+    return render_template('all_users.html', users=users)
 
 
 if __name__ == "__main__":
